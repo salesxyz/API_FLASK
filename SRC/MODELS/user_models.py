9ecd7cd066ec0df2a8070b9358925dac306eee3c
salesxyz <vh051208@gmail.com>
2026-08-05 11:54:42 -0300

    upd 2

---
+++ SRC/MODELS/user_models.py
@@ -0,0 +1,24 @@
+from connection import db
+from passlib.context import CryptContext
+
+
+class Usuario(db.Model):
+    __tablename__ == "usuarios"
+ 
+    id = Column(Interger, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    senha = Column(String(255), nullable=False)
+
+
+    pwd_context = CryptContext(schemes=['argon2'], deprecated='auto')
+
+
+    def gen_senha(self, senha):
+        self.senha = self.pwd_context.hash(senha)
+
+
+    def verifica_senha(self, senha):
+        return self.pwd_context.verify(senha, self.senha)
+    
+
